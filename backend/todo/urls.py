720b295f308ecdc8d305a4069d2111a8aef4efb5
jed <jmadjanor6@gmail.com>
2021-05-06 16:35:41 +0000

    First commit

---
+++ backend/todo/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from todo import views
+
+
+app_name = 'todo'
+
+
+
+urlpatterns = [
+    
+]
+
+
+
